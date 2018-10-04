1f258fe72b02fb0add25dc8550a69cc5ff0e09c3
Gabriel <gabrielmunits@gmail.com>
2018-10-05 01:55:08 +0300

    Kafka server using shopify/sarama get package. Added admin package to manage producers and consumers on sudo level

---
+++ kafka-server/publish.go
@@ -0,0 +1,10 @@
+package main
+
+// publishes rider's map location as producer message
+func Publish() {
+
+	// bin/kafka-console-producer.sh --broker-list localhost:9092 --topic location
+	//	> {long:38.895100,lat:-77.036400,city:"Washington DC"}
+	//  > {long:34.781769,lat:32.085300,city:"Tel Aviv"}
+	//  > {long:34.989571,lat:32.794044,city:"Haifa"}
+}
